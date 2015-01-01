543da8857f9e1d14740010749c88a224057de0e6
Lutz Vieweg <lvml@5t9.de>
2015-01-01 21:07:36 +0100

    * "cancellable" parameter fixed

--- xrandr.lua
+++ xrandr.lua
@@ -25,8 +25,8 @@ function xrandr_detect_available_rates()
 	-- invoke xrandr to find out which fps rates are available on the currently used output
 	
 	local p = {}
+	p["cancellable"] = "false"
 	p["args"] = {}
-	p["args"]["cancellable"] = "false"
 	p["args"][1] = "xrandr"
 	p["args"][2] = "-q"
 	local res = utils.subprocess(p)
